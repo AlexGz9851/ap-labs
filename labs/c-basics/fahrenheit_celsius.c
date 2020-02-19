3018d0d0dbcbef06e4c51d7022f93e9e037b907a
alexgz9851 <zzz12300212@gmail.com>
2020-02-18 20:39:04 -0600

    fahrenheit to celsius

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
-/* print Fahrenheit-Celsius table */
-
-int main()
-{
-    int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+int main(int argc, char** argv){
+    if(argc==2){
+        int f = atoi(argv[1]);
+        printf("Farenheit: %3i    Celsius: %3.1f\n",f, (5.0/9.0)*(f-32.0));
+        return 0;
+    }else{
+        for(int i=atoi(argv[1]);i<atoi(argv[2]);i+=atoi(argv[3])){
+            printf("Farenheit: %3i    Celsius: %3.1f\n",i, (5.0/9.0)*(i-32.0));
+        }
+    }
 }
