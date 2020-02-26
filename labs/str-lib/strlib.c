71b8421e29ad0392437d4e26aa1adccca7ea23dd
alexgz9851 <zzz12300212@gmail.com>
2020-02-25 20:54:26 -0600

    strlib.c a00820225

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
 
 int mystrlen(char *str){
-    return 0;
-}
-
-char *mystradd(char *origin, char *addition){
-    return 0;
+    int count=0;
+    while(str[count]!='\0') count++; 
+    return count;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int found = 1;
+    int lenOr = mystrlen(origin);
+    int lenS = mystrlen(substr);
+    if(lenOr < lenS) return 1;
+    for(int i=0; i<lenOr+1-lenS;i++){
+        found = 1;
+        for(int j=0;j<lenS;j++){
+            if(substr[j]!=origin[j+i]){
+                found = 0;
+                break;
+            }
+        }
+        if(found)return 0;
+    }
+    return 1;
+}
+
+char *mystradd(char *origin, char *addition){
+    int lenOrigin, lenAddition;
+    lenAddition = mystrlen(addition);
+    lenOrigin = mystrlen(origin);
+    char * result = (char *) malloc((lenOrigin+lenAddition+1)*sizeof(char *));
+    int i=0;
+    while(i<lenOrigin){
+        result[i] = origin[i];
+        i++;
+    }
+    while(i-lenOrigin<lenAddition){
+        result[i] = addition[i-lenOrigin];
+        i++;
+    }
+    result[lenAddition+lenOrigin] = '\0';
+    return result;
+}
+
+int main(int argc, char** argv){
+    printf("len %d\n",mystrlen(mystradd(argv[1], argv[2])));
+    printf("%s\n",mystradd(argv[1], argv[2]));
+    printf("%s\n", mystrfind(argv[1], argv[2])<0?"No":"Yes");
 }
