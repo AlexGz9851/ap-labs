62d019181473887dd16fffb9ec797b6712a369b5
alexgz9851 <zzz12300212@gmail.com>
2020-03-07 22:17:05 -0600

    maps and slices

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+    words := make(map[string]int)
+	for _, val := range strings.Fields(s){
+		words[val]++
+	}
+	return words
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,18 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	matrix := make([][]uint8, dx)
+	for i:=0;i<dx;i++{
+		matrix[i] = make([]uint8, dy)
+		for j:=0;j<dy;j++{
+			matrix[i][j] = uint8(i*i+j*j)
+		}
+	}
+	return matrix
+}
+
 func main() {
+	pic.Show(Pic)
 }
