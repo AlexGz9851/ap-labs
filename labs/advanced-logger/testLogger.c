bdd52e7ff54c04fbfbef7e9ab28d2549df3117a7
alexgz9851 <zzz12300212@gmail.com>
2020-04-08 21:10:33 -0500

    advanced-logger

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -4,21 +4,23 @@
 int main(){
 
     // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
 
     // stdout logging
     initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
+    panicf("ERROR Message %d\n", 32);
 
     // syslog logging
     initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 11);
+    warnf("WARN Message %d\n", 22);
+    errorf("ERROR Message %d\n", 32);
+    panicf("Panic at the syslog %d\n", 32);
 
     return 0;
 }
